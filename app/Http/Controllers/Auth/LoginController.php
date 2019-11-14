2da5197eeb2e1c0707e9a1c59859190890746b97
Thomas Leclerc <thomas.c.m@hotmail.fr>
2019-11-14 08:02:42 +0100

    Fake css + divers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/tdb';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
