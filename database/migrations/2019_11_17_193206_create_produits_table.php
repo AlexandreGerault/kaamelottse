bd6bc03451c990394d7a6c1fb3b6849782573791
Thomas Leclerc <thomas.c.m@hotmail.fr>
2019-11-17 21:11:19 +0100

    Ajout table produits

---
+++ database/migrations/2019_11_17_193206_create_produits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string('nom');
+			$table->text('description')->nullable();
+			$table->string('image')->nullable();
+			$table->integer('priorite')->default(0);
+			$table->integer('stock')->default(0);
+			$table->integer('prix')->default(0);
+			$table->integer('points')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produits');
+    }
+}
