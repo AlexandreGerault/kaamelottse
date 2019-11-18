acf3448575a9d2054d1d0f9196d7cf3b5c20ee93
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-18 22:20:51 +0100

    Add ProductsTableSeeder to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-//        $this->call(ArticlesTableSeeder::class);
+        $this->call(ArticlesTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
     }
 }
