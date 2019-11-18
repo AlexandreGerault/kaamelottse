eeacae468126e43586fb151dac0318e4a15b8ff0
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-18 19:59:17 +0100

    Adding some seeders to get some defaults permissions

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -11,6 +11,17 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('permissions')->insert([
+            ['name' => 'message'],
+            ['name' => 'message.index'],
+            ['name' => 'message.view'],
+            ['name' => 'message.viewany'],
+            ['name' => 'message.edit'],
+            ['name' => 'message.update'],
+            ['name' => 'message.delete'],
+            ['name' => 'message.forcedelete'],
+            ['name' => 'message.restore'],
+            ['name' => 'message.respond'],
+        ]);
     }
 }
