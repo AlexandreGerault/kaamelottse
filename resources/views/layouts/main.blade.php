e9eb8312cde2799bf773899586a9a6d50147f1ea
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-19 16:03:52 +0100

    Update views for display enhancements

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,7 @@
           href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" />
     <link rel="stylesheet" href="/style.css">
 	<!-- <link rel="stylesheet" href="/style-fake.css"> -->
     
