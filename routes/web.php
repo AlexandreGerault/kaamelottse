05b3941f345076efc38aabc6d8e13045832a201d
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-19 16:03:30 +0100

    Add order controller

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,16 @@ Route::get('/index', 'IndexController@index');
 Route::get('/contact', 'IndexController@get_contact');
 Route::post('/contact', 'IndexController@post_contact');
 Route::get('/tableRonde', 'IndexController@tableRonde');
-Route::get('/banquet', 'CommandeController@index');
+Route::get('/banquet', 'OrderController@index');
 
 Auth::routes();
 
 Route::get('/tableau-de-bord', 'HomeController@index')->name('dashboard');
-Route::get('/commander', 'CommandeController@index');
-
-Route::resource('article', 'ArticleController');
-Route::resource('message', 'MessagesController');
 Route::get('message/respond/{message}', 'MessagesController@respond')->name('message.respond');
 Route::post('message/respond/{message}', 'MessagesController@postRespond')->name('message.respond');
+
+Route::resource('order', 'OrderController');
+Route::resource('article', 'ArticleController');
+Route::resource('message', 'MessagesController');
 Route::resource('citation', 'CitationsController');
 Route::resource('product', 'ProductController');
