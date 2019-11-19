b661ea7abea8b4d032cc98c60f9db9e5f6d5be6e
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-19 16:05:56 +0100

    Create default seeders

---
+++ database/seeds/StockOperationsFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StockOperationsFactory extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
