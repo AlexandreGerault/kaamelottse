2dd84ef313f3f25837fff66d5e315e60658f18d5
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-19 20:52:33 +0100

    Changing seeder order (User BEFORE other that needs users)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CitationTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(CitationTableSeeder::class);
         $this->call(ArticlesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
         $this->call(OrdersTableSeeder::class);
