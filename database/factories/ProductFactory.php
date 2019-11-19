414cee317b6bb9e5aa7a79dec1c76aad42de3770
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-19 16:01:37 +0100

    Added factories to ProductSeeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->title(),
+        'description' => $faker->paragraph,
+        'image' => $faker->imageUrl('200', '200'),
+        'priority' => $faker->randomNumber(),
+        'stock' => $faker->randomNumber(),
+        'price' => $faker->randomFloat(2, 1, 20),
+        'points' => $faker->randomNumber(),
+        'available' => $faker->boolean,
+    ];
+});
