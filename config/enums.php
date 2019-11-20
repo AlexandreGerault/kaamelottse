9b82d6f7206f1c9acacb0bc858718621d8a0d344
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-20 23:28:37 +0100

    Update enums

--- config/enums.php
+++ config/enums.php
@@ -3,11 +3,11 @@
 return [
     'order' => [
         'status' => [
-            config('ordering.status.NOT_COMPLETED') => 'Non confirmée',
-            config('ordering.status.PENDING') => 'En attente de livreur',
-            config('ordering.status.IN_DELIVERY') => 'En livraison',
-            config('ordering.status.DELIVERED') => 'Livrée',
-            config('ordering.status.CANCELLED') => 'Annulée',
+            0 => 'Non confirmée',
+            1 => 'En attente de livreur',
+            2 => 'En livraison',
+            3 => 'Livrée',
+            4 => 'Annulée',
         ]
     ]
 ];
