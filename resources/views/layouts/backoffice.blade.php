dc9c0d9b103aa7758ce4e41be7cca00c554caac0
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-24 19:21:05 +0100

    Add logout to backoffice

--- resources/views/layouts/backoffice.blade.php
+++ resources/views/layouts/backoffice.blade.php
@@ -441,10 +441,13 @@
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-body">Cliquer sur le bouton de déconnexion pour vous déconneter.</div>
             <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a>
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
+                <form method="POST" action="{{ route('logout')}}">
+                    @csrf
+                    <input class="btn btn-primary" type="submit" value="Déconnexion">
+                </form>
             </div>
         </div>
     </div>
