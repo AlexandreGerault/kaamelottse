d6fef0dcfbf9f8ac44c6a2e2f770f3bc3356dba5
Alexandre Gérault <gerault-alexandre@orange.fr>
2019-11-26 18:49:20 +0100

    Update stock

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -27,18 +27,27 @@ class OrderObserver
     {
         $order->selfUpdateTotals();
 
+
+    }
+
+    public function updating(Order $order)
+    {
         /*
          * If the status field of the command has changed & is now equals to PENDING then
          */
-        if ($order->isDirty('status') && $order->status == config('ordering.status.PENDING')) {
+        if ($order->isDirty('status') && $order->getDirty()['status'] == config('ordering.status.PENDING')) {
             $order->items()->each(function (OrderItem $item) {
                 $product = $item->product;
-                $product->stock -= $item->quantity;
-                $product->save();
+                if ($product->stock - $item->quantity < 0) {
+                    return false;
+                }
+                else {
+                    $product->stock -= $item->quantity;
+                    $product->save();
+                }
             });
         }
     }
-
     /**
      * Handle the order "deleted" event.
      *
